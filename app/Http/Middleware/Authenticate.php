4e9dff50a7eb40b13cb0323481480d0b995ab18c
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-25 15:17:13 +0200

    modified auth middleware to return json error

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,33 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
-     */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        $authResult = $this->authenticate($request, $guards);
+        if ($authResult === "authentication_error") {
+            return response()->json(["error" => "Unauthorized"]);
         }
+
+        return $next($request);
+    }
+
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        return 'authentication_error';
     }
 }
