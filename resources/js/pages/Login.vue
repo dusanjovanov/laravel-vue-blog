857e0c7ef454101943bc46be5a7fd6f627ddca56
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-26 21:36:06 +0200

    login page functional

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,16 +1,53 @@
 <template>
- <div class="mt-4">
+  <div class="mt-4">
     <h1 class="mb-3">Welcome back!</h1>
-      <form>
-  <div class="form-group">
-    <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+    <form @submit.prevent="login" method="post">
+      <div class="form-group">
+        <label for="email">Email address</label>
+        <input
+          v-model="email"
+          type="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input v-model="password" type="password" class="form-control" id="password" />
+      </div>
+      <button type="submit" class="btn btn-primary">Log in</button>
+    </form>
   </div>
-  <div class="form-group">
-    <label for="password">Password</label>
-    <input type="password" class="form-control" id="password">
-  </div>
-  <button type="submit" class="btn btn-primary">Log in</button>
-</form>
- </div>
 </template>
+<script>
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      this.$http({
+        url: "auth/login",
+        method: "POST",
+        data: {
+          email: this.email,
+          password: this.password
+        }
+      })
+        .then(res => {
+          this.setUser(res.data.user);
+          this.$router.push({ path: "/dashboard" });
+        })
+        .catch(() => {
+          console.log("error");
+        });
+    },
+    ...mapActions(["setUser"])
+  }
+};
+</script>
