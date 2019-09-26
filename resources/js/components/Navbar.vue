42c2c9617d54fbd6d42c62d0a55221eecf56788b
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-26 16:28:25 +0200

    navbar links

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,28 +1,29 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
+    <router-link to="/" class="navbar-brand">Laravel/Vue Blog</router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarResponsive"
+      aria-controls="navbarResponsive"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav w-100">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/">All Posts</router-link>
+        </li>
+        <li class="ml-auto mr-2">
+          <router-link to="/register" class="btn btn-primary">Sign up</router-link>
+        </li>
+        <li>
+          <router-link to="/login" class="btn btn-secondary">Log in</router-link>
+        </li>
+      </ul>
     </div>
   </nav>
-</template>
+</template> 
