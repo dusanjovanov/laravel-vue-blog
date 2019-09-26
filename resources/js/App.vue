6350ba85fe62f07b3ca15804eac042c9ae3ba179
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-26 21:37:45 +0200

    check if user logged in and set user in vuex

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,21 +1,30 @@
 <template>
-    <div>
-        <navbar />
-        <div class="container page-container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <router-view />
-                </div>
-            </div>
+  <div>
+    <navbar />
+    <div class="container page-container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <router-view />
         </div>
-        <my-footer />
+      </div>
     </div>
+    <my-footer />
+  </div>
 </template>
 <script>
 export default {
-    components: {
-        navbar: require('./components/Navbar').default,
-        'my-footer': require('./components/Footer').default
-    }
-}
+  created() {
+    this.$http({
+      url: "auth/user"
+    })
+      .then(res => {
+        this.$store.dispatch("setUser", res.data.user);
+      })
+      .catch(err => console.log(err));
+  },
+  components: {
+    navbar: require("./components/Navbar").default,
+    "my-footer": require("./components/Footer").default
+  }
+};
 </script>
