85a958b3206a99db15eb8c499be38cc34a752be7
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-26 13:41:31 +0200

    vue axios, router config...

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,18 @@
-require("./bootstrap");
+import VueAuth from "@websanova/vue-auth";
+import axios from "axios";
 import Vue from "vue";
+import VueAxios from "vue-axios";
 import VueRouter from "vue-router";
+import auth from "./auth";
 import router from "./router";
+require("./bootstrap");
 
+Vue.router = router;
 Vue.use(VueRouter);
+
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = `${process.env.MIX_APP_URL}/api`;
+Vue.use(VueAuth, auth);
 
 new Vue({
     components: { app: require("./App.vue").default },
