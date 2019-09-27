2cf8f72ae0ca5812f7d2de477bd7782f585035c3
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-27 14:59:50 +0200

    hide/show links auth, dashboard link, style...

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <router-link to="/" class="navbar-brand">Laravel/Vue Blog</router-link>
+    <router-link to="/" class="navbar-brand ml-3">Laravel/Vue Blog</router-link>
     <button
       class="navbar-toggler"
       type="button"
@@ -15,15 +15,33 @@
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav w-100">
         <li class="nav-item active">
-          <router-link class="nav-link" to="/">All Posts</router-link>
-        </li>
-        <li class="ml-auto mr-2">
-          <router-link to="/register" class="btn btn-primary">Sign up</router-link>
+          <router-link to="/" exact class="nav-link">All Posts</router-link>
         </li>
         <li>
-          <router-link to="/login" class="btn btn-secondary">Log in</router-link>
+          <router-link v-if="user" to="/dashboard" class="nav-link">Dashboard</router-link>
+        </li>
+        <li class="ml-auto mr-2">
+          <router-link v-if="!user" to="/register" class="btn btn-primary">Sign up</router-link>
+        </li>
+        <li>
+          <router-link v-if="!user" to="/login" class="btn btn-secondary">Log in</router-link>
+        </li>
+        <li v-if="user" class="d-flex align-items-center mr-2">
+          <span class="text-white">{{user.name}}</span>
+        </li>
+        <li>
+          <button v-if="user" class="btn btn-secondary">Log out</button>
         </li>
       </ul>
     </div>
   </nav>
-</template> 
+</template>
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  }
+};
+</script>
