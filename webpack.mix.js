ebcd29b58572e8cf7e9f406eaf4b21241ae17f9e
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-29 13:08:51 +0200

    added source maps

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+    .sourceMaps()
+    .webpackConfig({ devtool: "cheap-module-eval-source-map" })
+    .sass("resources/sass/app.scss", "public/css");
 
 mix.disableNotifications();
