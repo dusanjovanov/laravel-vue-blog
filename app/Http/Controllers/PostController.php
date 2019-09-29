acf152913de718ded77a2cc974534a4fe6a15972
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-29 13:09:53 +0200

    route handling for adding a new post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -15,6 +16,33 @@ class PostController extends Controller
     public function index()
     {
         return ["posts" => Post::orderBy("created_at", "desc")->get()];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "title" => "required|min:1",
+            "body" => "required|json",
+            "featured_image" => "required|image",
+            "author_id" => "required|integer"
+        ]);
+
+        $post = new Post;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        // store file
+        $imagePath = Storage::disk('public')->putFile('images', $request->file("featured_image"));
+        $post->featured_image_url = '/storage/' . $imagePath;
+        $post->author_id = $request->author_id;
+        $post->save();
+
+        return ["post" => $post];
     }
 
     /**
