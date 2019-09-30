e0e96bf6294b4ae4c09f2088e04b3c00de0789b4
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-09-30 17:33:35 +0200

    added pagination of posts

--- clients/app/src/pages/Home.vue
+++ clients/app/src/pages/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1 class="my-4">All Posts</h1>
-    <!-- Blog Post -->
     <div class="card mb-4" v-for="post in posts" :key="post.id">
       <img class="card-img-top" :src="post.featured_image_url" :alt="post.title" />
       <div class="card-body">
@@ -12,14 +11,12 @@
         class="card-footer text-muted"
       >Posted on {{new Date(post.created_at).toLocaleDateString()}} by {{post.author.name}}</div>
     </div>
-
-    <!-- Pagination -->
     <ul class="pagination justify-content-center mb-4">
-      <li class="page-item">
-        <a class="page-link" href="#">&larr; Older</a>
+      <li :class="['page-item', {disabled: this.lastPage === null}]">
+        <button class="page-link" @click.prevent="getOlderPosts">&larr; Older</button>
       </li>
-      <li class="page-item disabled">
-        <a class="page-link" href="#">Newer &rarr;</a>
+      <li :class="['page-item', {disabled: this.nextPage === null}]">
+        <button class="page-link" @click.prevent="getNewerPosts">Newer &rarr;</button>
       </li>
     </ul>
   </div>
@@ -29,20 +26,34 @@ import { API_URL } from "../config";
 export default {
   data() {
     return {
-      posts: null
+      posts: null,
+      lastPage: null,
+      nextPage: null
     };
   },
   mounted() {
-    this.getPosts();
+    this.getPosts(1);
   },
   methods: {
-    getPosts() {
-      fetch(`${API_URL}/posts`)
+    getPosts(page) {
+      return fetch(`${API_URL}/posts?limit=5&page=${page}`)
         .then(res => res.json())
         .then(data => {
-          this.posts = data.posts;
+          this.posts = data.data;
+          (this.lastPage =
+            data.current_page === 1 ? null : data.current_page - 1),
+            (this.nextPage =
+              data.current_page === data.last_page
+                ? null
+                : data.current_page + 1);
         })
         .catch(err => console.log(err));
+    },
+    getNewerPosts() {
+      this.getPosts(this.nextPage).then(() => window.scrollTo(0, 0));
+    },
+    getOlderPosts() {
+      this.getPosts(this.lastPage).then(() => window.scrollTo(0, 0));
     }
   }
 };
