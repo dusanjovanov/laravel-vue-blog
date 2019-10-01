91dfe568c1e64a9fc7494fcc4790ec4dc74a8f1a
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-10-01 18:26:58 +0200

    posts table component

---
+++ clients/dashboard/src/components/PostsTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <table v-if="posts" class="table table-striped table-bordered table-responsive">
+    <thead>
+      <th></th>
+      <th>ID</th>
+      <th>TITLE</th>
+      <th>AUTHOR</th>
+      <th>IMAGE</th>
+      <th>BODY</th>
+      <th>POSTED AT</th>
+      <th>LAST UPDATED</th>
+    </thead>
+    <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td>
+          <input
+            @change="selectRow"
+            type="checkbox"
+            name="select_row"
+            id="select_row"
+            :data-id="post.id"
+            style="width: 1.3em; height: 1.3em;"
+          />
+        </td>
+        <td>{{post.id}}</td>
+        <td>{{post.title}}</td>
+        <td>{{post.author.name}}</td>
+        <td>
+          <img :src="post.featured_image_url" style="height: 50px;" />
+        </td>
+        <td>{{post.body.substring(0,30) + '...'}}</td>
+        <td style="white-space:nowrap;">{{new Date(post.created_at).toLocaleString()}}</td>
+        <td style="white-space:nowrap;">{{new Date(post.updated_at).toLocaleString()}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  props: ["posts", "selectRow"]
+};
+</script>
