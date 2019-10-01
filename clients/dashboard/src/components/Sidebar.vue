4a25f79c6ed229d8e9bb4c19c28ca7f36d0307b4
Dusan Jovanov <jovanovdusan1@gmail.com>
2019-10-01 12:37:44 +0200

    cleaned up the theme and split into comp.

---
+++ clients/dashboard/src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <div class="sidebar-brand-text mx-3">Laravel/Vue</div>
+    </a>
+    <hr class="sidebar-divider my-0" />
+    <li class="nav-item active">
+      <a class="nav-link" href="index.html">
+        <span>Dashboard</span>
+      </a>
+    </li>
+    <hr class="sidebar-divider" />
+    <div class="sidebar-heading">Resources</div>
+    <li class="nav-item">
+      <a
+        class="nav-link collapsed"
+        href="#"
+        data-toggle="collapse"
+        data-target="#collapseTwo"
+        aria-expanded="true"
+        aria-controls="collapseTwo"
+      >
+        <span>Posts</span>
+      </a>
+    </li>
+    <hr class="sidebar-divider d-none d-md-block" />
+    <div class="text-center d-none d-md-inline">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+  </ul>
+</template>
